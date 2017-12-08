4ef7bdba9a59f600fefc538f5a3e408c2101e38b
Marko <marko.k.89@gmail.com>
2017-12-08 14:55:44 +0100

    Counter

--- src/Counter.js
+++ src/Counter.js
@@ -1,13 +1,51 @@
 import React, { Component } from 'react';
 
 export default class Counter extends Component {
+  state = {
+    count: 0
+  }
+  /*
+   constructor() {
+     super();
+ 
+     this.state = {
+       count: 54
+     };
+ 
+     // this.hanfleIncrement = this.hanfleIncrement.bind(this);
+   }
+ */
+
+  // strelica zamenjuje bind. (bind-ovanje metode)
+  handleIncrement = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
+
+  // strelica zamenjuje bind. (bind-ovanje metode)
+  handleDecrement = () => {
+    this.setState({
+      count: this.state.count - 1
+    });
+  }
+
+  // strelica zamenjuje bind. (bind-ovanje metode)
+  handleReset = () => {
+    this.setState({
+      count: 0
+    });
+  }
+
+
   render() {
+    const { count } = this.state;
     return (
       <section className="Counter">
-        <h1>Count: {0}</h1>
-        <button onClick={() => {}} className="full-width">Increment</button>
-        <button onClick={() => {}} className="full-width">Decrement</button>
-        <button onClick={() => {}} className="full-width">Reset</button>
+        <h1>Count: {count}</h1>
+        <button onClick={this.handleIncrement} className="full-width">Increment</button>
+        <button onClick={this.handleDecrement} className="full-width">Decrement</button>
+        <button onClick={this.handleReset} className="full-width">Reset</button>
       </section>
     );
   }
